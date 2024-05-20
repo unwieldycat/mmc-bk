052a9f626a797f3458d3336405733d76c269584a
Thurston Yates <thurstonalexyates@gmail.com>
2024-05-19 23:01:39 -0400

    Move helper functions into own file

--- src/mod.ts
+++ src/mod.ts
@@ -1,43 +1,5 @@
-import { relative, resolve } from "@std/path";
-import { walk } from "@std/fs/walk";
-import { BlobWriter, ZipWriter } from "@zip-js/zip-js";
-
-// =============================== Functions =============================== //
-
-async function createZipFromDir(
-	dirPath: string,
-	outPath: string,
-	fileName: string,
-) {
-	// Create zip writers
-	const zipFileWriter = new BlobWriter("application/zip");
-	const zipWriter = new ZipWriter(zipFileWriter);
-
-	for await (const dirEntry of walk(dirPath)) {
-		if (!dirEntry.isFile) continue;
-
-		const filePath = relative(dirPath, dirEntry.path);
-		Deno.open(dirEntry.path, { read: true, write: false })
-			.then((file) => {
-				zipWriter.add(filePath, file.readable);
-			});
-	}
-
-	// Close writer and save to zip file
-	zipWriter.close().then((zipBlob) => {
-		Deno.create(
-			resolve(outPath, fileName + ".zip"),
-		).then((file) => {
-			zipBlob.stream().pipeTo(file.writable);
-		});
-	});
-}
-
-function getModifiedDate(filePath: string): Date {
-	return Deno.statSync(filePath).mtime || new Date(0);
-}
-
-// ================================== Main ================================== //
+import { resolve } from "@std/path";
+import { createZipFromDir, getModifiedDate } from "./utils.ts";
 
 // Get Environment Variables
 const instanceName = Deno.env.get("INST_NAME");
---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+import { walk } from "@std/fs/walk";
+import { BlobWriter, ZipWriter } from "@zip-js/zip-js";
+import { relative, resolve } from "@std/path";
+
+export async function createZipFromDir(
+	dirPath: string,
+	outPath: string,
+	fileName: string,
+) {
+	// Create zip writers
+	const zipFileWriter = new BlobWriter("application/zip");
+	const zipWriter = new ZipWriter(zipFileWriter);
+
+	for await (const dirEntry of walk(dirPath)) {
+		if (!dirEntry.isFile) continue;
+
+		const filePath = relative(dirPath, dirEntry.path);
+		Deno.open(dirEntry.path, { read: true, write: false })
+			.then((file) => {
+				zipWriter.add(filePath, file.readable);
+			});
+	}
+
+	// Close writer and save to zip file
+	zipWriter.close().then((zipBlob) => {
+		Deno.create(
+			resolve(outPath, fileName + ".zip"),
+		).then((file) => {
+			zipBlob.stream().pipeTo(file.writable);
+		});
+	});
+}
+
+export function getModifiedDate(filePath: string): Date {
+	return Deno.statSync(filePath).mtime || new Date(0);
+}
